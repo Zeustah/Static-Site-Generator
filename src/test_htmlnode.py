d9b41cde4f1c897ef76879322034f8c7ad9ebba5
Zeustah <ar8358@gmail.com>
2026-01-09 14:52:36 -0500

    (4) added LeafNode child class, methods, and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -22,6 +22,30 @@ class TestHTMLNode(unittest.TestCase):
         )
         self.assertEqual(node2.props_to_html(), "")
         self.assertEqual(node3.props_to_html(), ' href="https://www.google.com"')
+
+    def test_leaf_to_html_p(self):
+        node1 = LeafNode("p", "Hello, world!")
+        self.assertEqual(node1.to_html(), "<p>Hello, world!</p>")
+
+        node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node2.to_html(), '<a href="https://www.google.com">Click me!</a>'
+        )
+
+        node3 = LeafNode(None, "Just text")
+        self.assertEqual(node3.to_html(), "Just text")
+
+        node4 = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node4.to_html()
+
+        node5 = LeafNode(
+            "a", "Double props", {"href": "https://boot.dev", "target": "_blank"}
+        )
+        self.assertEqual(
+            node5.to_html(),
+            '<a href="https://boot.dev" target="_blank">Double props</a>',
+        )
 
 
 if __name__ == "__main__":
